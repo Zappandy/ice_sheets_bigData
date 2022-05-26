2b210fe80820affc03949f1f3ef52afd5b80674b
Andres <nanoandres_24@hotmail.com>
2022-05-26 16:10:58 +0200

    removed redundant cassandra compose files

--- cassandra/testing_cass/cassandra_init.sh
+++ cassandra/testing_cass/cassandra_init.sh
@@ -1,5 +1,4 @@
 CQL="CREATE KEYSPACE IF NOT EXISTS test_tables WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};"
-
 until echo $CQL | cqlsh; do
 	  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
 	    sleep 2
