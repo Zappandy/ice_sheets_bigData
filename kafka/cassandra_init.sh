748efdf98360dd129a17cd021812e364bc8c3620
Andres <nanoandres_24@hotmail.com>
2022-05-28 08:41:30 +0200

    improved table syntax

--- kafka/cassandra_init.sh
+++ kafka/cassandra_init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
+#TBT="CREATE TABLE icesheetreport(_year INT, _month INT, _day INT, extend FLOAT, missing FLOAT, hemisphere TEXT) PRIMARY KEY (hemisphere, extend);"
+# primary key is to identify a row
 #CQL="CREATE KEYSPACE IF NOT EXISTS icesheet_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};"
 CQL="CREATE KEYSPACE IF NOT EXISTS icesheet_keyspace
      WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};
-     CREATE TABLE IF NOT EXISTS icesheetreport(_year INT,
-     _month INT, _day INT, extend FLOAT, missing FLOAT,
-     hemisphere TEXT) PRIMARY KEY (hemisphere, extend);
+     USE icesheet_keyspace;
+     CREATE TABLE IF NOT EXISTS icesheetreport(year INT, month INT, day INT,
+     extend FLOAT, missing FLOAT, hemisphere TEXT PRIMARYKEY)
+     PRIMARY KEY (hemisphere);
      "
-#TBT="CREATE TABLE icesheetreport(_year INT, _month INT, _day INT, extend FLOAT, missing FLOAT, hemisphere TEXT) PRIMARY KEY (hemisphere, extend);"
 until echo $CQL | cqlsh; do
 	  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
 	    sleep 2
