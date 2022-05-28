dc1488b2c2b224a96bbd2788e15b733b6b4414f9
Andres <nanoandres_24@hotmail.com>
2022-05-28 07:35:19 +0200

    removed redundant dockerfile, updated shell for cassandra, and removed build for connect in compose

--- kafka/cassandra_init.sh
+++ kafka/cassandra_init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-CQL="CREATE KEYSPACE IF NOT EXISTS icesheet_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};"
+#CQL="CREATE KEYSPACE IF NOT EXISTS icesheet_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};"
+CQL="CREATE KEYSPACE IF NOT EXISTS icesheet_keyspace
+     WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};
+     CREATE TABLE IF NOT EXISTS icesheetreport(_year INT,
+     _month INT, _day INT, extend FLOAT, missing FLOAT,
+     hemisphere TEXT) PRIMARY KEY (hemisphere, extend);
+     "
 #TBT="CREATE TABLE icesheetreport(_year INT, _month INT, _day INT, extend FLOAT, missing FLOAT, hemisphere TEXT) PRIMARY KEY (hemisphere, extend);"
 until echo $CQL | cqlsh; do
 	  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
