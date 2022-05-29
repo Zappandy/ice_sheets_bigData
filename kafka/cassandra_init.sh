6858b56c10b109c2b2cfd4d71e281c387fef5c83
Andres <nanoandres_24@hotmail.com>
2022-05-29 09:24:47 +0200

    fixed case insensitive headers

--- kafka/cassandra_init.sh
+++ kafka/cassandra_init.sh
@@ -9,8 +9,8 @@ CQL="DROP KEYSPACE IF EXISTS icesheet_keyspace;
      CREATE KEYSPACE icesheet_keyspace
      WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};
      USE icesheet_keyspace;
-     CREATE TABLE icesheetreport(Year INT, Month INT, Day INT,
-     Extent FLOAT, Missing FLOAT, Hemisphere TEXT, PRIMARY KEY (Hemisphere));
+     CREATE TABLE icesheetreport(\"Year\" INT, \"Month\" INT, \"Day\" INT,
+     \"Extent\" FLOAT, \"Missing\" FLOAT, \"Hemisphere\" TEXT, PRIMARY KEY (\"Hemisphere\"));
      "
 until echo $CQL | cqlsh; do
 	  echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
