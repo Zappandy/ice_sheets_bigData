38895676070c9f6571b38f68bb8404d306e11287
Andres <nanoandres_24@hotmail.com>
2022-05-30 13:22:14 +0200

    loaded up df, but gunicorn can be slow, increase ram in container?

--- visualization/app/app.py
+++ visualization/app/app.py
@@ -29,8 +29,6 @@ app.title = 'Icesheets_Dashboard'
 #session.set_keyspace(keyspace)
 #session.execute('SELECT * FROM icesheetreport')
 
-# print(session.execute('SELECT * FROM icesheetreport'))
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 # stream connections for predictions and visualizations
 
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
@@ -46,68 +44,74 @@ consumer = KafkaConsumer(LISTEN_TO_TOPICS, group_id="raw_streams",
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
 def num_records(consum, n=1000):
+    multiple_streams = []
     for i, msg in enumerate(consum):
-        print(f"{msg.topic}, {msg.partition}, {msg.offset}, {msg.key}, {msg.value}")
-        print(f"{type(msg.topic)}, {type(msg).partition}, {type(msg).offset}, {type(msg).key}, {type(msg).value}")
+        multiple_streams.append(msg.value)
         if i == 30:
             break
         # records = consum.poll(n*5)
-    # return list(records.values())[0]
+    return multiple_streams
 
 # define a key
-elms = num_records(consumer)
-#icesheet_df = pd.DataFrame.from_records([el.value for el in elms])
+
+icesheet_df = pd.DataFrame.from_records(num_records(consumer))
+print(icesheet_df.empty)
+print(icesheet_df.head(10))
+print(icesheet_df.shape)
 #mask_df = icesheet_df["Hemisphere"] == 'N'
 #north_df = icesheet_df[mask_df]
 #south_df = icesheet_df[~mask_df]
 
-#for i, msg in enumerate(consumer):
-#    print(f"{msg.topic}, {msg.partition}, {msg.offset}, {msg.key}, {msg.value}")  # year, month, day, extend, missing, hemisphere
+
 # extension = north_df["Extent"].tolist()
 # years = north_df["Year"].tolist()
 # months = north_df["Month"].tolist()
 
-# heatmap = px.imshow([extension]) 
+# heatmap = px.imshow([extension])
 #print(icesheet_df.head(10))
+if icesheet_df.empty:
+    print("can't enter yet...")
 
-fig_heatmap = go.Figure(data=go.Heatmap(
-          x=[i for i in range(1, 30)],
-          y=[i for i in range(1, 12)],
-          z=[5, 14, 8],
-          type = 'heatmap',
-          colorscale = 'Viridis'))
+else:
+    print("entering...")
+    fig_heatmap = go.Figure(data=go.Heatmap(
+              x=[i for i in range(1, 30)],
+              y=[i for i in range(1, 12)],
+              z=[5, 14, 8],
+              type = 'heatmap',
+              colorscale = 'Viridis'))
 
-app.layout = dbc.Container([
-  
-    dbc.Row(dbc.Col(html.H2("Icesheet Dashboard"), width={'size': 12, 'offset': 0, 'order': 0}), style={'textAlign': 'center', 'paddingBottom': '1%'}),
-  
-    # dbc.Row(dbc.Col(dcc.Loading(children=[dcc.Graph(id ='your-graph'),
-    #                                     dcc.Slider(id='year-slider',
-    #                                                 min=df['year'].min(),
-    #                                                 max=df['year'].max(),
-    #                                                 value=df['year'].min(),
-    #                                                 marks={str(year): str(year) for year in df['year'].unique()},
-    #                                                 step=None)
-    #                                     ], color = '#000000', type = 'dot', fullscreen=True ) )),
-dbc.Row(dbc.Col(dcc.Loading(
-    children=[dcc.Graph(id='north_extension', figure=fig_heatmap)], style={'width': '49%', 'display': 'inline-block' })))
-])
-  
-@app.callback(
-    Output('your-graph', 'figure'),
-    Input('year-slider', 'value'))
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-  
-    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                    size="pop", color="continent", hover_name="country",
-                    log_x=True, size_max=55)
-  
-    fig.update_layout(transition_duration=500)
-  
-    return fig
+    app.layout = dbc.Container([
 
-  
+        dbc.Row(dbc.Col(html.H2("Icesheet Dashboard"), width={'size': 12, 'offset': 0, 'order': 0}), style={'textAlign': 'center', 'paddingBottom': '1%'}),
+
+        # dbc.Row(dbc.Col(dcc.Loading(children=[dcc.Graph(id ='your-graph'),
+        #                                     dcc.Slider(id='year-slider',
+        #                                                 min=df['year'].min(),
+        #                                                 max=df['year'].max(),
+        #                                                 value=df['year'].min(),
+        #                                                 marks={str(year): str(year) for year in df['year'].unique()},
+        #                                                 step=None)
+        #                                     ], color = '#000000', type = 'dot', fullscreen=True ) )),
+    dbc.Row(dbc.Col(dcc.Loading(
+        children=[dcc.Graph(id='north_extension', figure=fig_heatmap)], style={'width': '49%', 'display': 'inline-block' })))
+    ])
+
+    @app.callback(
+        Output('your-graph', 'figure'),
+        Input('year-slider', 'value'))
+    def update_figure(selected_year):
+        filtered_df = df[df.year == selected_year]
+
+        fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                        size="pop", color="continent", hover_name="country",
+                        log_x=True, size_max=55)
+
+        fig.update_layout(transition_duration=500)
+
+        return fig
+
+
 if __name__=='__main__':
      app.run_server()
      #app.run_server(host='0.0.0.0:80', port=5000, debug=True)
